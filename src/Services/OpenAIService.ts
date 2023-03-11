6ee74f47317fb0e2daefeffc236a2008250cdced
陳鈞 <jim60105@gmail.com>
2023-03-12 03:15:25 +0800

    Change prompt

--- src/Services/OpenAIService.ts
+++ src/Services/OpenAIService.ts
@@ -20,6 +20,7 @@ export class OpenAIService {
         formData.set('model', 'whisper-1');
         formData.set('response_format', 'verbose_json');
         formData.set('temperature', '0.4');
+        formData.set('prompt', 'This audio is a cut from a live stream. Please remove duplicate words. It may not be in English, please try to identify it.');
 
         return from(
             fetch(OpenAIService.OpenAIEndpoint + 'audio/transcriptions', {
@@ -64,7 +65,7 @@ export class OpenAIService {
          */
         const raw = JSON.stringify({
             model: 'text-davinci-003',
-            prompt: `This passage is a speech-to-text input, there may be inaccuracies, so please make your own guesses about the possible near-sounding words. Please use ${browser_language} to summarize the main idea of the passage. And then Generate 5 responses in ${browser_language} and ${audioResponse.language}. The tone should be casual and use popular internet slang as much as possible. Each response should not exceed 30 tokens. The \`stream_language\` field is the language of the speech to text input. Responses are strictly required in the following json format! \`\`\` { "stream_language": "${audioResponse.language}",  "summary": "這篇文章的總結",   "responses": [     {       "${audioResponse.language}": "これが最初の可能な返信です",       "${browser_language}": "這是第一個可能的回覆"     },     {       "${audioResponse.language}": "問題ない",       "${browser_language}": "沒問題"     },     {       "${audioResponse.language}": "草",       "${browser_language}": "草"     }   ] } \`\`\`  Please read this passage and answer: """${audioResponse.text}"""`,
+            prompt: `This passage is a raw speech-to-text input from a live stream. There may be inaccuracies, so please make your own guesses about the possible near-sounding words. Please remove duplicate words. Please use ${browser_language} to summarize the main idea of the passage. And then Generate 5 responses in one set in ${browser_language} and ${audioResponse.language}. The tone should be casual and use popular internet slang as much as possible. Each response should not exceed 30 tokens. The \`stream_language\` field is the language of the speech to text input. Responses are strictly required in the following valid json format!! \`\`\` { "stream_language": "${audioResponse.language}",  "summary": "這篇文章的總結",   "responses": [     {       "${audioResponse.language}": "これが最初の可能な返信です",       "${browser_language}": "這是第一個可能的回覆"     },     {       "${audioResponse.language}": "問題ない",       "${browser_language}": "沒問題"     },     {       "${audioResponse.language}": "草",       "${browser_language}": "草"     }   ] } \`\`\`  Please read this passage and answer: """${audioResponse.text}"""`,
             temperature: 1,
             max_tokens: 2500,
         });
