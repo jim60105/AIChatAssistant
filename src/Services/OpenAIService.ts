e9a4a11fcbf1046a62a0bde17cdafe08571180ff
陳鈞 <jim60105@gmail.com>
2023-12-18 22:54:54 +0800

    chore: Update OpenAI to use `gpt-3.5-turbo-1106` and experiment with JSON mode.
    https://platform.openai.com/docs/guides/text-generation/json-mode
    
    - Changed the OpenAI language model from "text-davinci-003" to "gpt-3.5-turbo-1106"
    - Updated the API endpoint URL to include "/chat/completions" instead of just "/completions"
    - Modified the `raw` object to use the new model and response format
    - Replaced the `fetch` URL to match the updated endpoint URL
    
    Signed-off-by: 陳鈞 <jim60105@gmail.com>

--- src/Services/OpenAIService.ts
+++ src/Services/OpenAIService.ts
@@ -60,29 +60,26 @@ export class OpenAIService {
 
         /**
          * Models available for this api:
-         * text-davinci-003
-         * text-davinci-002: Performance is very bad, give it up.
-         * text-curie-001
-         * text-babbage-001
-         * text-ada-001
-         * davinci
-         * curie
-         * babbage
-         * ada
          *
          * NOTE: Any model with less than 3000 tokens is not suitable for our situation
-         * https://platform.openai.com/docs/models/model-endpoint-compatability
+         * https://platform.openai.com/docs/guides/text-generation
          */
         const raw = JSON.stringify({
-            model: 'text-davinci-003',
-            prompt: `This is a transcription of a live stream that has been converted from speech to text. Please note that some words may be incorrect due to the conversion process, so use your own judgment to figure out what the speaker meant. Ignore any parts of the passage that are repeated, and guess what's missing if necessary. Using ${browserLanguage}, summarize the main idea of the passage. Then, generate ${responseCount} casual and slangy responses in both ${browserLanguage} and ${streamLanguage}, each no longer than 30 tokens. Be careful not to misplace the language in the responses. Responses are strictly required in the following valid json format!! \`\`\` { "summary": "這篇文章的總結",   "responses": [     {       "${streamLanguage}": "これが最初の可能な返信です",       "${browserLanguage}": "這是第一個可能的回覆"     },     {       "${streamLanguage}": "問題ない",       "${browserLanguage}": "沒問題"     },     {       "${streamLanguage}": "草",       "${browserLanguage}": "草"     }   ] } \`\`\`  Please read this passage and answer: """${speechToText.text}"""`,
-
+            model: 'gpt-3.5-turbo-1106',
+            response_format: { type: 'json_object' },
+            messages: [
+                {
+                    role: 'system',
+                    content: `This is a transcription of a live stream that has been converted from speech to text. Please note that some words may be incorrect due to the conversion process, so use your own judgment to figure out what the speaker meant. Ignore any parts of the passage that are repeated, and guess what's missing if necessary. Using ${browserLanguage}, summarize the main idea of the passage. Then, generate ${responseCount} casual and slangy responses in both ${browserLanguage} and ${streamLanguage}, each no longer than 30 tokens. Be careful not to misplace the language in the responses. Responses are strictly required in the following valid json format!! \`\`\` { "summary": "這篇文章的總結",   "responses": [     {       "${streamLanguage}": "これが最初の可能な返信です",       "${browserLanguage}": "這是第一個可能的回覆"     },     {       "${streamLanguage}": "問題ない",       "${browserLanguage}": "沒問題"     },     {       "${streamLanguage}": "草",       "${browserLanguage}": "草"     }   ] } \`\`\``,
+                },
+                { role: 'user', content: speechToText.text },
+            ],
             temperature: 1,
             max_tokens: 2000,
         });
 
         return from(
-            fetch(OpenAIService.OpenAIEndpoint + 'completions', {
+            fetch(OpenAIService.OpenAIEndpoint + 'chat/completions', {
                 method: 'POST',
                 headers: {
                     Authorization: `Bearer ${this.apiKey}`,
@@ -97,7 +94,9 @@ export class OpenAIService {
                 console.debug('Get response: %o', response);
                 const responseText =
                     /(\{.+\})/.exec(
-                        response.choices[0].text?.replaceAll('\n', '').replaceAll('`', '') ?? ''
+                        response.choices[0].message?.content
+                            ?.replaceAll('\n', '')
+                            .replaceAll('`', '') ?? ''
                     )?.[1] ?? '';
 
                 return JSON.parse(responseText);
